b20e73c8f9bf5a9233ef0f4d93b05222846dc6bb
Romain Deville <code@romaindeville.fr>
2022-11-27 10:47:10 +0100

    ✨ Add temporary code to instanciate git provider

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as gitProvider from "./providers"
+
+// Get configuration
+const config = new pulumi.Config();
+const gitProviders = config.requireObject<gitProvider.IProviders>("gitProvider");
+
+for (const i_gitProviderName in gitProviders) {
+  const provider = gitProviders[i_gitProviderName]
+
+  const currProvider = gitProvider.providerFactory(
+    provider["type"],
+    i_gitProviderName,
+    provider);
+  pulumi.log.error(currProvider.name)
+}
+
