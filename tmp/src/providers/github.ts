3651833b77349cdea0fb75e16b9d312e770d5ec7
Romain Deville <code@romaindeville.fr>
2023-01-15 00:21:30 +0100

    🔥 Remove old files

--- tmp/src/providers/github.ts
+++
@@ -1,9 +0,0 @@
-import {Provider} from "./providers";
-
-export class GithubProvider extends Provider {
-
-    public constructor (name: string, data: Partial<Provider>) {
-        super(name, data);
-    }
-
-}
--- tmp/src/providers/gitlab.ts
+++
@@ -1,9 +0,0 @@
-import {Provider} from "./providers";
-
-export class GitlabProvider extends Provider {
-
-    public constructor (name: string, data: Partial<Provider>) {
-        super(name, data);
-    }
-
-}
