514563cc980fca4262247e1937cdb43a4621a75f
Romain Deville <code@romaindeville.fr>
2023-01-30 22:21:50 +0100

    ✅ Add project pipeline trigger support tests

--- tests/projects/utils.ts
+++ tests/projects/utils.ts
@@ -534,3 +534,37 @@ test("project with supported provider with deployTokens", (currTest) => {
             deployTokens.fakeDeployTokenName.urn
     );
 });
+
+
+test("project with supported provider with pipelineTriggers", (currTest) => {
+    const fakeProjects: project.ProjectsPulumiInfo = {
+        "fakeProjectName": {
+            "desc": PROJECT_DESC,
+            "pipelineTriggers": {
+                "fakePipelineTriggerName": {
+                    "description": "Fake pipeline trigger description"
+                }
+            },
+            // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+            "providers": [PROVIDER_NAME[0]]
+        }
+    };
+
+    const providers = provider.initProvider(PROVIDER);
+    project.initProject(
+        providers,
+        fakeProjects
+    );
+
+    currTest.regex(
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        providers[PROVIDER_NAME[0]].projects.fakeProjectName.name,
+        /fakeprojectname-[A-Za-z0-9]{5}/u
+    );
+    currTest.snapshot(
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        providers[PROVIDER_NAME[0]].
+            projects.fakeProjectName.
+            pipelineTriggers.fakePipelineTriggerName.urn
+    );
+});
